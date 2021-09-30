346c591c3671ba2c0fbe2394f6ee1f1ffe6cf996
aqilat94 <aqilat94@gmail.com>
2021-09-30 15:37:29 +0800

    change controller name

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
-Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+Route::get('auth/google', [App\Http\Controllers\SocialiteController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [App\Http\Controllers\SocialiteController::class, 'handleGoogleCallback']);

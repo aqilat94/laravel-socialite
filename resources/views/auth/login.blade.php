d747035872e7da9b3c9b9e0525cd34304a7d72d8
aqilat94 <aqilat94@gmail.com>
2021-09-30 15:37:10 +0800

    add google and github login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,6 +64,16 @@
                                 @endif
                             </div>
                         </div>
+                        <div class="flex items-center justify-end mt-4">
+                            <a href="{{ url('auth/google') }}">
+                                <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+                            </a>
+                        </div>
+                        <div class="flex items-center justify-end mt-4">
+                            <a href="{{ url('auth/google') }}">
+                                <img src="https://github.githubassets.com/images/modules/open_graph/github-mark.png" style="margin-left: 3em;">
+                            </a>
+                        </div>
                     </form>
                 </div>
             </div>

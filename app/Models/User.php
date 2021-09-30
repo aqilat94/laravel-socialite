2668fd7df478d4de8f49fd6d00030abafa04e0ba
aqilat94 <aqilat94@gmail.com>
2021-09-30 15:36:08 +0800

    discard unuse import

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,9 +13,7 @@ class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
-    use HasProfilePhoto;
     use Notifiable;
-    use TwoFactorAuthenticatable;
   
     /**
      * The attributes that are mass assignable.

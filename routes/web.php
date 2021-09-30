3624b3e84772b36e355accc491d0cde65ab3fa9b
aqilat94 <aqilat94@gmail.com>
2021-09-30 14:58:58 +0800

    add google auth route\

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
